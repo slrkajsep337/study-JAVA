9396fbb0e2379bf8ec16a975cf769b990a7bfbe3
이수진 <slrkajsep337@naver.com>
2022-10-06 01:35:53 +0900

    Interface 실습해보기

---
+++ src/InterfacePrac/EvenOddMain.java
@@ -0,0 +1,17 @@
+package InterfacePrac;
+
+
+public class EvenOddMain {
+    public static void main(String[] args) {
+
+        //객체 생성 + 클래스1 대입
+        CalculateNums cct = new CalculateNums(new EvenOddKorean());
+        cct.GetResult(5,6);
+
+        //생성된 객체에 클래스2 대입
+        cct = new CalculateNums(new EvenOddEnglish());
+        cct.GetResult(5,6);
+
+
+    }
+}
---
+++ src/InterfacePrac/Number.java
@@ -0,0 +1,8 @@
+package InterfacePrac;
+
+//문자열 출력 메소드를 인터페이스에 선언
+//-> 이 인터페이스를 가지고 두가지 클래스를 구현(EvenOddKorean/ EvenOddEnglish)
+public interface Number {
+    public String getNum(int a);
+}
+
