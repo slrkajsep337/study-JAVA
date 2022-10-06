ac5a4601597772a3cccf08764623041e6d78b2f8
이수진 <slrkajsep337@naver.com>
2022-10-06 15:13:51 +0900

    file 목록 출력하기

---
+++ src/day3/ReadFile.java
@@ -0,0 +1,26 @@
+package day3;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+//파일을 읽어들일 버퍼 객체 생성
+
+public class ReadFile {
+    public static void main(String[] args) throws IOException {
+
+//        FileReader reader = new FileReader("a_file.txt");
+//        int ch;
+//        while ((ch = reader.read()) != -1) {
+//            System.out.print((char) ch);
+
+        File dir = new File("./");
+        File files[] = dir.listFiles();
+        for(File file: files) {
+            System.out.println(file);
+    }
+    }
+
+}
