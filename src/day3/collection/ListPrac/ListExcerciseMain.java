4442b7f029b7215fc972c4f58ab9a2673bc1ac7f
이수진 <slrkajsep337@naver.com>
2022-10-06 20:56:09 +0900

    ListExercise_주석추가

--- src/day3/collection/ListPrac/ListExcerciseMain.java
+++ src/day3/collection/ListPrac/ListExcerciseMain.java
@@ -6,14 +6,16 @@ import java.util.List;
 public class ListExcerciseMain {
     public static void main(String[] args) {
 
+        //객체 생성
         ListExercise listExercise = new ListExercise();
         List<String> students = listExercise.getSutdents();
 
-
+        //for-each문으로 어레이리스트의 값을 하나씩 꺼내 출력
         for (String student : students) { //listExcercise.getStudents();
             System.out.println(student);
         }
 
+        //students 리스트의 사이즈 반환
         System.out.println(students.size());
     }
 
--- src/day3/collection/ListPrac/ListExercise.java
+++ src/day3/collection/ListPrac/ListExercise.java
@@ -5,14 +5,18 @@ import java.util.List;
 
 public class ListExercise {
 
+
+    //외부에서 접근할 수 없도록 멤버변수는 private으로 선언
     private List<String> students = new ArrayList<>();
 
+    //위에서 선언한 students를 초기화해주는 생성자
     public ListExercise() {
         this.students = new ArrayList<>();
         this.students.add("김경록");
         this.students.add("가나다");
     }
 
+    //private으로 선언된 students에 접근하기 위한 method 생성
     public List<String> getSutdents() {
         return this.students;
     }
