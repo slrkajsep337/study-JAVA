ee35621102a0fdc116c54b784ec0fae5d5cebe40
이수진 <slrkajsep337@naver.com>
2022-10-07 22:05:56 +0900

    population statistics project 실습

--- src/day4/PopulationStatisticsProject/PopulationMove.java
+++ src/day4/PopulationStatisticsProject/PopulationMove.java
@@ -1,29 +1,24 @@
-package day4;
+package day4.PopulationStatisticsProject;
 
+//우리가 받아온 데이터 파일에서 원하는 값만 추출하는 메소드
 public class PopulationMove {
-    private long id;
+
     private int fromSido;
     private int toSido;
 
-    //constructor 추가 fromSido, toSIdo를 받아서 멤버변수에 넣는 기능
+    //1. constructor 추가 - fromSido, toSido를 받아서 멤버변수에 넣는 기능
     public PopulationMove(int fromSido, int toSido) {
         this.fromSido = fromSido;
         this.toSido = toSido;
     }
 
+    //3. 위에 int로 선언된 멤버변수들이 배열에들어가면서 string이 되어서 다시 int로 오버로딩해준다
     public PopulationMove(String fromSido, String toSido) {
         this.fromSido = Integer.parseInt(fromSido);
         this.toSido = Integer.parseInt(toSido);
     }
 
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
+    //private에 접근할 수 있도록 getter생성
     public int getFromSido() {
         return fromSido;
     }
@@ -31,6 +26,5 @@ public class PopulationMove {
     public int getToSido() {
         return toSido;
     }
-
 
 }
--- src/day4/PopulationStatisticsProject/PopulationStatistics.java
+++ src/day4/PopulationStatisticsProject/PopulationStatistics.java
@@ -1,89 +1,73 @@
-package day4;
+package day4.PopulationStatisticsProject;
 
 import java.io.*;
-import java.nio.Buffer;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class PopulationStatistics {
-    public void readByChar(String filename) throws IOException{
-        FileReader fileReader = new FileReader(filename);
 
-        String fileContents = "";
-        while (fileContents.length() < 1_000_000) {
-            char c = (char) fileReader.read();
-            fileContents += c;
-            System.out.println(fileContents);
-        }
-        System.out.println(fileContents);
-    }
-
-//    public List<PopulationMove> readByLine(String filename) throws IOException {
-//
-//        List<PopulationMove> pml = new ArrayList<>();
+//    public void readByLine(String filename) throws IOException {
+//        //BufferedReader의 매개변수에는 파일이름을 읽어들일 수 있는
+//        // 객체(FileReader)만 들어올 수 있다
 //        BufferedReader reader = new BufferedReader(
 //                new FileReader(filename));
-//
 //        String str = "";
 //        while((str = reader.readLine()) != null) {
 ////            System.out.println(str);
-//            PopulationMove pm = PopulationMoveParse(str);
-//            pml.add(pm);
+//            PopulationMove pm = parse(str);
 //        }
 //        reader.close();
-//        return pml;
 //    }
 
-    public List<PopulationMove> readFileByLineV2(String filename) throws IOException {
-
+    //위의 메소드를 리스트에 데이터를 넣어주기 위한 메소드로 바꾸어준다
+    // (리스트를 생성하고 거기에 데이터를 넣어줌)
+    public List<PopulationMove> readByLine(String filename) throws IOException {
         List<PopulationMove> pml = new ArrayList<>();
-        BufferedReader br = new BufferedReader(
+        //BufferedReader의 매개변수에는 파일이름을 읽어들일 수 있는
+        // 객체(FileReader)만 들어올 수 있다
+        BufferedReader reader = new BufferedReader(
                 new FileReader(filename));
         String str = "";
-        //String을 PopulationMove로 파싱하여 pml에 add
-        while ((str = br.readLine()) != null) {
-            pml.add(parse(str));
+        while((str = reader.readLine()) != null) {
+//            System.out.println(str);
+            PopulationMove pm = parse(str);
+            pml.add(pm);
         }
-        br.close();
+        reader.close();
         return pml;
     }
 
+    // 2. ,을 기준으로 데이터를 분리해주는 메소드
     public PopulationMove parse(String data) {
+        //분리해서 배열에 넣기
+        //배열에 들어가면서 data값이 string으로 바뀐다 -> 위에 생성자 오버로딩해줘야함
+        String[] SidoData = data.split(",");
+        //return new PopulationMove(sidoData[6],sidoData[0]);
+        //원하는 값만 추출한 데이터를 from_to파일로 새로 생성해주었다-> 필요한 정보인덱스 0,1
+        return new PopulationMove(SidoData[0], SidoData[1]);
 
-//        String[] arr = data.split(",");
-//        return new PopulationMove(arr[0], arr[6]);
-        String[] splittedLine = data.split(",");
-        return new PopulationMove(splittedLine[0], splittedLine[1]);//from, to
     }
 
-
-    //data에서 시도를 찾아 Map에 저장하는 메소드
-    public void setMapsByLine(String filename, PopulationMoveMap populationMoveMap) {
-        try (BufferedReader br = Files.newBufferedReader(Paths.get(filename), StandardCharsets.UTF_8)) {
-            String line;
-            for (int i = 0; i < 10; i++) {
-                line = br.readLine();
-                populationMoveMap.save(parse(line));
-            }
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
+    //파일을 생성하는 메소드
     public void createAFile(String filename) throws IOException {
         File file = new File(filename);
         file.createNewFile();
+        //파일을 생성할 때 어떤 방식으로든 예외처리는 해줘야함
+//        try {
+//            file.createNewFile();
+//        } catch (IOException e) {
+//            throw new RuntimeException(e);
+//        }
     }
-
+    //생성된 파일에 내용을 write해주는 메소드
     //List<String>을 지정한 파일에 write
     public void write(List<String> strs, String filename) {
         File file = new File(filename);
+        //파일에 내용을 작성해줄 때에도 반드시 예외처리를 해주어야 한다
         try {
+            //BufferedWriter와 FileWriter를 사용하여 작성할 객체 생성
             BufferedWriter writer = new BufferedWriter(new FileWriter(file));
             for(String str: strs) {
                 writer.write(str);
@@ -94,10 +78,11 @@ public class PopulationStatistics {
         }
     }
 
-    public String fromToString(PopulationMove populationMove) {
-        return populationMove.getFromSido()+","+populationMove.getToSido()+"\n";
-    }
-
+    //1 키 만들기
+    //2 map에서 키에 해당하는 밸류가 없으면 1 대입
+    //3 map에서 키에 해당하는 밸류가 있으면 +1
+    //4 리턴
+    //전입,전출의 횟수를 세서 map에 넣어주는 메소드
     public Map<String, Integer> getMoveCntMap(List<PopulationMove> pml) {
         Map<String, Integer> moveCntMap = new HashMap<>();
         for(PopulationMove pm : pml) {
@@ -111,14 +96,16 @@ public class PopulationStatistics {
         return  moveCntMap;
     }
 
-
+    public String fromToString(PopulationMove populationMove) {
+        return populationMove.getFromSido()+","+populationMove.getToSido()+"\n";
+    }
 
     public static void main(String[] args) throws IOException {
         String address = "/Users/leesuujeen/Downloads/2021_인구관련연간자료_20220927_66125.csv";
         PopulationStatistics populationStatistics = new PopulationStatistics();
         PopulationStatistics populationStatistics2 = new PopulationStatistics();
-        List<PopulationMove> pml = populationStatistics.readFileByLineV2(address);
-        List<PopulationMove> pml2 = populationStatistics.readFileByLineV2(address);
+        List<PopulationMove> pml = populationStatistics.readByLine(address);
+        List<PopulationMove> pml2 = populationStatistics.readByLine(address);
 
 //        List<String> strings = new ArrayList<>();
 //        strings.add("11,11");
@@ -127,7 +114,7 @@ public class PopulationStatistics {
 //            strings.add(fromTo);
 //        }
 //        populationStatistics.write(strings, "./from_to.txt");
-
+//
 //        for (PopulationMove pm : pml) {
 //            System.out.printf("전입:%s, 전출:%s\n", pm.getFromSido(), pm.getToSido());
 //        }
@@ -143,7 +130,7 @@ public class PopulationStatistics {
             //System.out.printf("key : %s value : %d\n",key, map.get(key));
         }
         populationStatistics2.write(cntResult, targetFilename);
-
     }
+
 
 }
