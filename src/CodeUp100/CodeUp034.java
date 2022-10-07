193a7d8b7362ec0fbfb5a013ec8ffe105fb02d07
이수진 <slrkajsep337@naver.com>
2022-10-07 23:39:34 +0900

    code up 100제 031~040

---
+++ src/CodeUp100/CodeUp034.java
@@ -0,0 +1,12 @@
+package CodeUp100;
+
+import java.util.Scanner;
+
+public class CodeUp034 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = Integer.valueOf(sc.next(),8);
+
+        System.out.println(num);
+    }
+}
---
+++ src/CodeUp100/CodeUp036.java
@@ -0,0 +1,12 @@
+package CodeUp100;
+
+import java.util.Scanner;
+
+public class CodeUp036 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        char c = sc.next().charAt(0);
+
+        System.out.println((int)c);
+    }
+}
