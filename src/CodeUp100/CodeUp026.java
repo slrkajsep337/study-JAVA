1ebe41c752f0a0c47403519e884c6d4080352dc4
이수진 <slrkajsep337@naver.com>
2022-10-07 23:17:11 +0900

    자바에서 문자열 비교는 .equals() 주의**

--- src/CodeUp100/CodeUp026.java
+++ src/CodeUp100/CodeUp026.java
@@ -2,14 +2,16 @@ package CodeUp100;
 
 import java.util.Scanner;
 
-//해결안됨
+
 public class CodeUp026 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String time = sc.nextLine();
         String[] arr = time.split(":");
 
-        if(arr[1] == "00") {
+        //자바에서 문자열 비교는 "=="가 아닌 .equals()
+        //==는 주소값을 비교한다
+        if(arr[1].equals("00")) {
             System.out.println("0");
         } else {
             System.out.println(arr[1]);
