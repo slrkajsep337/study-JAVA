1ca7b14696d8b507a16cc940029241b38bd5ae03
이수진 <slrkajsep337@naver.com>
2022-10-14 13:59:34 +0900

    query 생성 method 수정

--- readline-parser/src/main/java/com/line/Main.java
+++ readline-parser/src/main/java/com/line/Main.java
@@ -4,20 +4,25 @@ import com.line.domain.Hospital;
 import com.line.parser.HospitalParser;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        LineReaderRefactor<Hospital> hospitalFileController = new LineReaderRefactor<>(new HospitalParser());
+        LineReaderRefactor<Hospital> lrr = new LineReaderRefactor<>(new HospitalParser());
         String filename = "/Users/leesuujeen/Desktop/서울시 병의원 위치 정보.csv";
-        List<Hospital> hospitals = hospitalFileController.readLines(filename);
+        List<Hospital> hospitals = lrr.readLines(filename);
 
         System.out.println(hospitals.size());
+        List<String> lines = new ArrayList<>();
+        lines.add("INSERT INTO `hospital-db`.`seoul-hospital`\n"+
+                "(`id`,`address`,`district`,`category`,`emergencyroom`,`name`,`subdivision`)\n"+
+                "VALUES\n");
         for (Hospital hospital : hospitals) {
-            System.out.printf("%s,%s,%s,%s,%d,%s,%s\n",
-                    hospital.getId(),hospital.getAddress(), hospital.getDistrict()
-                    ,hospital.getCategory(), hospital.getEmergencyRoom(), hospital.getName(),
-                    hospital.getSubdivision());
+            lines.add(hospital.getTupleString());
         }
-    }
+        String sqlFilename = "hospital_insert.sql";
+        lrr.createANewFile(sqlFilename);
+        lrr.writeLines(lines, sqlFilename);
+        }
 }
--- readline-parser/src/main/java/com/line/domain/Hospital.java
+++ readline-parser/src/main/java/com/line/domain/Hospital.java
@@ -1,6 +1,9 @@
 package com.line.domain;
 //T타입에 들어갈 객체들을 domain 패키지에 넣어 정리한다
 
+import java.io.*;
+import java.util.List;
+
 //Hospital 오브젝트 생성
 //parser메소드로 추출한 필요한정보들을 Hospital객체에 넣는다
 public class Hospital {
@@ -8,22 +11,44 @@ public class Hospital {
     private String address;
     private String district;
     private String category;
-    private int emergencyRoom;
+    private Integer emergencyRoom;
     private String name;
     private String subdivision;
     //ctrl+n 으로 constructor 생성
 
 
     //constructor 생성
-    public Hospital(String id, String address, String district, String category, int emergencyRoom, String name, String subdivision) {
+    public Hospital(String id, String address, String district, String category, Integer emergencyRoom, String name, String subdivision) {
         this.id = id;                           //0
         this.address = address;                 //1
+        //district를 추출하는 첫번째 방법
         this.district = district;
+        //district를 추출하는 두번째 방법
 //        this.setDistrict();          //1
         this.category = category;               //2
         this.emergencyRoom = emergencyRoom;     //6
         this.name = name;                       //10
         this.subdivision = subdivision;         //10
+    }
+
+    public String getSqlInsertQuery() {
+        String sql = String.format(
+                "INSERT INTO `hospital-db`.`seoul-hospital`\n"+
+                "(`id`,`address`,`district`,`category`,`emergencyroom`,`name`,`subdivision`)\n"+
+                "VALUES\n"+
+                "('%s','%s','%s','%s',%s,'%s','%s')\n",
+                this.id, this.address, this.district, this.category,
+                this.emergencyRoom, this.name, this.subdivision);
+        return sql;
+    }
+
+
+    public String getTupleString() {
+        String sql = String.format(
+                "('%s','%s','%s','%s',%d,'%s','%s'),\n",
+                this.id, this.address, this.district, this.category,
+                this.emergencyRoom, this.name, this.subdivision);
+        return sql;
     }
 
     public String getId() {
