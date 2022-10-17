ce2d62b4698cccbc1ab68eb3e13eb2fe9ffeddfb
이수진 <slrkajsep337@naver.com>
2022-10-17 09:36:23 +0900

    star prac - constructor생성(모양선택가능)

--- src/Algorithm/StarPrac/StarPrac02.java
+++ src/Algorithm/StarPrac/StarPrac02.java
@@ -2,12 +2,21 @@ package Algorithm.StarPrac;
 
 public class StarPrac02 {
 
+    private String shape = "*";
+
+    public StarPrac02() {
+
+    }
+    public StarPrac02(String shape) {
+        this.shape = shape;
+    }
+
     private String star(int n) {
         String star="";
 
         for (int i=0; i<n; i++) {
             for(int j=n-i; j<n+1; j++) {
-                star += "*";
+                star += this.shape;
             }
             star+="\n";
         }
@@ -15,7 +24,7 @@ public class StarPrac02 {
     }
     public static void main(String[] args) {
 
-        StarPrac02 sp = new StarPrac02();
+        StarPrac02 sp = new StarPrac02("#");
         String rst = sp.star(4);
         System.out.println(rst);
 
