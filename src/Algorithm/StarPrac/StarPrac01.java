553fccbc1d04d63b97983a6e260173164c6eaef4
이수진 <slrkajsep337@naver.com>
2022-10-17 09:10:18 +0900

    star prac

---
+++ src/Algorithm/StarPrac/StarPrac01.java
@@ -0,0 +1,10 @@
+package Algorithm.StarPrac;
+
+public class StarPrac01 {
+    public static void main(String[] args) {
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+    }
+}
