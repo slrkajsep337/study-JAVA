a20138ed7b73fae65e92042cd7b9b0dda083f0ef
이수진 <slrkajsep337@naver.com>
2022-10-27 09:14:19 +0900

    Hash

--- Algorithm/src/main/java/datastructure/HashTableRunning.java
+++ Algorithm/src/main/java/datastructure/HashTableRunning.java
@@ -6,6 +6,13 @@ import java.util.Map;
 //프로그래머스 - 완주하지 못한 선수
 public class HashTableRunning {
 
+
+    //해쉬맵에 participant 원소를 키값으로, 해당 키값이 나올때마다 value++
+    //그리고 completion의 원소와 위에서 생성한 해쉬맵의 키값이 같다면, 해당키가 나올때마다 value--
+    //그러면 completion에 해당안되는 사람은 >0 값을 가지게 되고, 중복 키값도 처리 가능하다
+    //그 값만 골라서 출력
+
+
     class Solution {
         public String solution(String[] participant, String[] completion) {
             String answer = "";
