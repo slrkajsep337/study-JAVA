2a89bcbe7293a3b187fa9491bd7371cf10c7b460
이수진 <slrkajsep337@naver.com>
2022-10-28 10:49:13 +0900

    Hash 폰켓몬

--- Algorithm/src/main/java/datastructure/HashPhonketmon.java
+++ Algorithm/src/main/java/datastructure/HashPhonketmon.java
@@ -27,6 +27,30 @@ public class HashPhonketmon {
         return answer;
     }
 
+    public int solution2(int[] nums) {
+        int answer = 0;
+        int cnt = 0;
+
+        Map<Integer, Integer> hm = new HashMap<>();
+
+        for(Integer i: nums) {
+            hm.put(i, hm.getOrDefault(i, 0)+1);
+        }
+
+        for(Integer key: hm.keySet()) {
+            if(hm.get(key) > 0) {
+                cnt += 1;
+            }
+        }
+
+        if (cnt>nums.length/2) answer = nums.length/2;
+        else answer = cnt;
+
+        return answer;
+    }
+
+
+
     public static void main(String[] args) {
 
 
