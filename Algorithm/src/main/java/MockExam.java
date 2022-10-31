1a99aa0eeb3795f9450db57ded7bbcfd86d11fd6
이수진 <slrkajsep337@naver.com>
2022-11-01 00:27:08 +0900

    프로그래머스 - 모의고사

--- Algorithm/src/main/java/MockExam.java
+++ Algorithm/src/main/java/MockExam.java
@@ -1,44 +1,49 @@
-public class MockExam {
+import java.util.ArrayList;
+import java.util.Arrays;
 
-    class Solution {
-        public int[] solution(int[] answers) {
-            int[] answer = {};
+public class MockExam {
+        public ArrayList<Integer> solution(int[] answers) {
+            ArrayList<Integer> answer = new ArrayList<Integer>();
+            int[] stdnt = {0, 0, 0};
             int[] stu1answer = {1, 2, 3, 4, 5};
             int[] stu2answer = {2, 1, 2, 3, 2, 4, 2, 5};
             int[] stu3answer = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
-            int stu1 = 0;
-            int stu2 = 0;
-            int stu3 = 0;
 
-            for (int i=0; i<answer.length; i++) {
-                if (i>stu1answer.length){
-                    int j = i%stu1answer.length;
-                    if (stu1answer[j] == answer[i]){
-                        stu1 += 1;
-                    }
+            for (int i=0; i<answers.length; i++) {
+                if (stu1answer[i % stu1answer.length] == answers[i]) {
+                    stdnt[0] += 1;
+                }
+                if (stu2answer[i%stu2answer.length] == answers[i]) {
+                    stdnt[1] += 1;
+                }
+                if (stu3answer[i%stu3answer.length] == answers[i]) {
+                    stdnt[2] += 1;
                 }
             }
 
-            for (int i=0; i<answer.length; i++) {
-                if (i>stu2answer.length){
-                    int j = i%stu1answer.length;
-                    if (stu2answer[j] == answer[i]){
-                        stu2 += 1;
-                    }
-                }
+            int max = 0;
+            for (int s: stdnt) {
+                if(s > max) max = s;
             }
 
-            for (int i=0; i<answer.length; i++) {
-                if (i>stu3answer.length){
-                    int j = i%stu3answer.length;
-                    if (stu3answer[j] == answer[i]){
-                        stu3 += 1;
-                    }
+            int cnt = 0;
+            for (int i=0; i<3; i++) {
+                if (stdnt[i]==max) {
+                    answer.add(i+1);
+                    cnt += 1;
                 }
             }
 
             return answer;
         }
+
+        public static void main(String[] args) {
+
+            MockExam me = new MockExam();
+            int[] answers = {1,2,3,4,5};
+            for (int s: me.solution(answers)) {
+                System.out.println(s);
+            }
     }
 
 
