404f4d277fb4a9ebb4c9ff02afd194910d1208b7
이수진 <slrkajsep337@naver.com>
2022-11-01 09:29:44 +0900

    소수구하기 - 1번 방법

---
+++ Algorithm/src/main/java/FindingPrimenum.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+
+public class FindingPrimenum {
+
+    public static void main(String[] args) {
+
+        int[] nums = {13, 17, 19, 23};
+        ArrayList<Integer> arr = new ArrayList<Integer>();
+        boolean check = true;
+
+        for(int n: nums) {
+            for(int i=2; i<n; i++) {
+                if(n%i == 0) {
+                    check = false;
+                    break;
+                }
+            }
+            if(check == true) {
+                arr.add(n);
+            }
+        }
+
+        for(int a: arr) {
+            System.out.println(a);
+        }
+
+
+    }
+
+}
