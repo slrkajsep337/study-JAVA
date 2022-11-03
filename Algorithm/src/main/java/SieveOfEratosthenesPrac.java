a6bd54b82a3cf24625b9502977d01e5e6e128f5e
이수진 <slrkajsep337@naver.com>
2022-11-03 09:34:27 +0900

    2의 배수 출력하기

---
+++ Algorithm/src/main/java/SieveOfEratosthenesPrac.java
@@ -0,0 +1,11 @@
+public class SieveOfEratosthenesPrac {
+
+    public static void main(String[] args) {
+
+        int num = 2;
+
+        for(int i=0; i<101; i++) {
+            System.out.println(i+" "+num*(i+1));
+        }
+    }
+}
