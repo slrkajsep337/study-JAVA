3fb29079bf321fd4e3b800bc24a9338553da7d19
이수진 <slrkajsep337@naver.com>
2022-11-03 10:22:41 +0900

    에라토스테네스의체 연습 - true인 값만 출력(2제외하고 2의 배수가 아닌값)

--- Algorithm/src/main/java/SieveOfEratosthenesPrac.java
+++ Algorithm/src/main/java/SieveOfEratosthenesPrac.java
@@ -1,11 +1,22 @@
+import java.util.Arrays;
+
 public class SieveOfEratosthenesPrac {
 
     public static void main(String[] args) {
+        //에라토스테네스의 체 -> 핵심은 %, add(), remove() 연산을 하지 않는 것
 
-        int num = 2;
+        int N = 50; // N = 50
+        int[] nums = new int[N - 1]; // N-1개만큼 2~50까지 총 49개
+        boolean[] checks = new boolean[nums.length]; // nums만큼 checks생성
+        Arrays.fill(checks, true); // checks를 true로 초기화 cuz 기본값 false
 
-        for(int i=2; i<101; i++) {
-            System.out.println(i+" "+num*(i-1));
+
+        for (int i = 2; i < nums.length; i += 2) checks[i] = false;
+        for (int i = 0; i < nums.length; i++) nums[i] = i + 2; // 2~50까지 nums에 채우기
+
+        for (int i = 0; i < nums.length; i++) {
+            if (checks[i] == true) System.out.println(nums[i]);
         }
     }
+
 }
