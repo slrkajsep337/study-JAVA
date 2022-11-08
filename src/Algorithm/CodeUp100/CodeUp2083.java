1ed9e15d92df790bcfda64df5cfe3c71d0f606ec
이수진 <slrkajsep337@naver.com>
2022-11-08 10:23:46 +0900

    code up 2083 풀이중(이분탐색)

---
+++ src/Algorithm/CodeUp100/CodeUp2083.java
@@ -0,0 +1,23 @@
+package Algorithm.CodeUp100;
+
+import java.util.Scanner;
+
+public class CodeUp2083 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int s = sc.nextInt();
+        int[] arr = new int[n];
+
+        for (int i=0; i<n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int startidx = 0;
+        int endidx = n;
+        int mididx = (startidx+endidx)/2;
+
+
+
+    }
+}
