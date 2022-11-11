b3bf620e52b57d78a9f674bdda1dd64c442f5446
이수진 <slrkajsep337@naver.com>
2022-11-11 10:31:08 +0900

    프로그래머스 - 비밀지도, 시간초과

--- Algorithm/src/main/java/SecretMap.java
+++ Algorithm/src/main/java/SecretMap.java
@@ -2,17 +2,26 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class SecretMap {
-    public String[] solution(int n, int[] arr1, int[] arr2) {
-        String[] answer = {};
+    public static String[] solution(int n, int[] arr1, int[] arr2) {
+        String[] answer = new String[n];
         List<String> temp = new ArrayList<>();
-
-//        String binaryString = Integer.toBinaryString(i);
+        String temp1;
+        String temp2;
+        String answertemp = "";
+        int sum=0;
 
         for(int i=0; i<n; i++) {
-
+            temp1 = Integer.toBinaryString(arr1[i]);
+            temp2 = Integer.toBinaryString(arr2[i]);
+            sum = Integer.parseInt(temp1)+Integer.parseInt(temp2);
+            for(int j=0; j<n; j++) {
+                if(sum%10>0) answertemp = "#"+answertemp;
+                else answertemp = " "+answertemp;
+                sum /= 10;
+            }
+            answer[i] = answertemp;
+            answertemp = "";
         }
-
-
 
         return answer;
     }
@@ -33,8 +42,11 @@ public class SecretMap {
         }
 
         for(int i=binarynum.length()-1; i>-1; i--) {
-            System.out.print(binarynum.charAt(i));
+//            System.out.print(binarynum.charAt(i));
         }
+
+
+
 
     }
 }
